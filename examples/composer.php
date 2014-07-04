9c1e1c1230cfe6d90743866142eb23e52669eb17
Alexey Maslov <alexey.y.maslov@gmail.com>
2014-07-05 00:17:09 +0400

    composer example.

---
+++ examples/composer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * This program is free software. It comes without any warranty, to
+ * the extent permitted by applicable law. You can redistribute it
+ * and/or modify it under the terms of the Do What The Fuck You Want
+ * To Public License, Version 2, as published by Sam Hocevar. See
+ * http://www.wtfpl.net/ for more details.
+ *
+ * Usage example for composer
+ * @author alxmsl
+ * @date 10/22/12
+ */
+
+include '../vendor/autoload.php';
+
+use alxmsl\Connection\Redis\RedisFactory;
+
+// Create Redis Client instance with you configuration settings
+$Redis = RedisFactory::createRedisByConfig(array(
+    'host' => 'localhost',
+    'port' => 6379,
+));
+
+// Use Redis commands
+$Redis->set('test', '8');
+var_dump($Redis->get('test'));
