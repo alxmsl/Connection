c10b6977add9cb5be39897c22221432a91e6cce5
mkrasilnikov <mak.krasilnikov@gmail.com>
2015-11-05 11:47:36 +0300

    added inint instr escape functions for query templates

--- source/Postgresql/QueryTemplate.php
+++ source/Postgresql/QueryTemplate.php
@@ -87,4 +87,34 @@ final class QueryTemplate extends DbTemplate {
         return pg_escape_identifier((string) $table);
     }
 
+    /**
+     * Escape array values
+     * @param array $array
+     * @return string
+     */
+    public function arr(array $array) {
+        return $this->str(sprintf('{"%s"}', implode('", "', $array)));
+    }
+
+    /**
+     * Escape integers array
+     * @param array $array
+     * @return string
+     */
+    public function inint(array $array) {
+        $array = array_map('intval', $array);
+        return sprintf('(%s)', implode(', ', $array));
+    }
+
+    /**
+     * Escape strings array
+     * @param array $array
+     * @return string
+     */
+    public function instr(array $array) {
+        $array = array_map(function ($value) {
+            return pg_escape_literal((string) $value);
+        }, $array);
+        return implode(", ", $array);
+    }
 }
