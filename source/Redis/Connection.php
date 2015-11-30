38002d71d9b198d2cb7cb45c8db6a8c206461cb0
Alexey Maslov <alexey.y.maslov@gmail.com>
2015-11-30 23:42:46 +0300

    changed license to Apache-2.0.

--- source/Redis/Connection.php
+++ source/Redis/Connection.php
@@ -1,13 +1,22 @@
 <?php
 /*
- * This program is free software. It comes without any warranty, to
- * the extent permitted by applicable law. You can redistribute it
- * and/or modify it under the terms of the Do What The Fuck You Want
- * To Public License, Version 2, as published by Sam Hocevar. See
- * http://www.wtfpl.net/ for more details.
+ * Copyright 2015 Alexey Maslov <alexey.y.maslov@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 namespace alxmsl\Connection\Redis;
+
 use alxmsl\Connection\AbstractConnection;
 use alxmsl\Connection\Redis\Exception\ConnectException;
 use alxmsl\Connection\Redis\Exception\ImpossibleValueException;
@@ -22,7 +31,6 @@ use Redis;
 /**
  * Class for redis client
  * @author alxmsl
- * @date 11/1/12
  */
 final class Connection extends AbstractConnection implements RedisInterface {
     /**
